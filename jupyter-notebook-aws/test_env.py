b80a46f3cf9d18223d461b1c2d67ec15b85a76a0
Ubuntu <ubuntu@ip-172-31-12-114.eu-west-1.compute.internal>
2019-10-11 13:26:22 +0000

    add example

--- jupyter-notebook-aws/test_env.py
+++ jupyter-notebook-aws/test_env.py
@@ -4,6 +4,7 @@ findspark.init('/home/ubuntu/spark-2.4.4-bin-hadoop2.7')
 from pyspark import SparkConf, SparkContext
 
 sc = SparkContext(conf=SparkConf())
-lines = sc.textFile('/home/ubuntu/text.txt')
+lines = sc.textFile('/home/ubuntu/config-python/jupyter-notebook-aws/test.txt')
 rdd = lines.map(lambda line: line.split())
-print(rdd.collect()
+print(rdd.collect())
+
